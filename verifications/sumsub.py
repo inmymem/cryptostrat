5ad4f9e4a6dfa82f7f688e70408dc7557060a2e5
Charaf El Berdai <charaf.berdai@gmail.com>
2020-06-30 11:00:21 +0100

    t

--- verifications/sumsub.py
+++ verifications/sumsub.py
@@ -9,6 +9,7 @@ from .models import RawSumSubNotification, Application, IdDoc, Address, AddressD
 import telegram.bot as telegram
 from django.utils import timezone
 from .verification_links import get_verification_url
+from clients.models import Client
 
 
 # test_api_url = 'https://test-api.sumsub.com'
@@ -235,12 +236,10 @@ def process_notification(request):
             except Exception as e:
                 telegram.send_critical_error_message.delay(f'Sumsub - error creating Person for {client.uuid}')
             client.liveness_verified = is_liveness_verified(applicant_data)
-            print('hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii')
             if client.localbitcoins_real_name:
                 client.name_match_checked = do_names_match(client, person)
             else:
                 client.name_match_checked = True
-            print('hiddddddddddddddddddddddddddddddddddddddddd')
             ##############
             name_components = [person.first_name, person.middle_name, person.last_name]
             client.real_name = ''
